76526d6b35d73d087a766da9fcf56c195c068f54
Bryan Enders <endersstocker@users.noreply.github.com>
2018-04-15 08:43:06 -0400

    Port: SourceEvent
    Why
    ---
    
    Build a struct or pseudo-object (module that wraps an agent) in Elixir
    for every object in the Ruby implementation until Elixir implementation
    passes Cucumber’s acceptance tests
    
    How
    ---
    
    * Port SourceEvent
    * Create Media struct

---
+++ gherkin/elixir/lib/gherkin/source_event.ex
@@ -0,0 +1,25 @@
+defmodule Gherkin.SourceEvent do
+  alias Gherkin.Media
+
+  @type t :: %__MODULE__{
+          data: File.io_device(),
+          media: Media.t(),
+          type: String.t(),
+          uri: Path.t()
+        }
+
+  @enforce_keys [:data, :media, :type, :uri]
+  defstruct @enforce_keys
+
+  @spec stream([Path.t()]) :: Enumerable.t()
+  def stream(paths), do: Stream.map(paths, &event/1)
+
+  @spec event(Path.t()) :: t
+  defp event(path),
+    do: %__MODULE__{
+      data: File.open!(path, [:read, :utf8]),
+      media: %Media{encoding: "utf-8", type: "text/x.cucumber.gherkin+plain"},
+      type: "source",
+      uri: path
+    }
+end
